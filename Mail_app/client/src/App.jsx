84cdaabcc884744b6ce83e3d65a2dc9a5cb7fa0e
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-12 19:23:00 +0530

    issue can't admin and role based

--- Mail_app/client/src/App.jsx
+++ Mail_app/client/src/App.jsx
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Donate from './pages/DonateUs';
 import Contact from './pages/Contact';
-import Features from './pages/Features';
-import Pricing from './pages/Pricing';
-import Cart from './components/Cart';
 import Campaign from './pages/campaign/Campaign';
+import Pricing from './pages/Pricing';
+import Features from './pages/Features';
+import Cart from './components/Cart';
 import CampaignForm from './pages/campaign/CampaignForm';
 import CampaignsList from './pages/campaign/CampaignList';
 import EmailTemplateCreator from './pages/campaign/EmailTemplateCreator';
 import SelectTemplate from './pages/campaign/SelectTemplate';
 import Analytics from './pages/Analytics';
+import Admin from './pages/Admin';
+import ProtectedRoute from './components/ProtectedRoute';
+import { Roles } from './roles';
 
 const App = () => {
   return (
@@ -23,7 +25,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/donate" element={<Donate />} />
           <Route path="/campaign" element={<Campaign />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/features" element={<Features />} />
@@ -32,7 +33,15 @@ const App = () => {
           <Route path="/all-campaigns" element={<CampaignsList />} />
           <Route path="/create-template" element={<EmailTemplateCreator />} />
           <Route path="/select-template" element={<SelectTemplate />} />
-          <Route path="/analytics"  element={<Analytics/>} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRoute roles={[Roles.ADMIN]}>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
         <Footer />
       </div>
