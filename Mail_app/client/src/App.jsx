592637bf80fbcac402c5304d756fbae3d48f4205
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-19 07:33:24 +0530

    changes

--- Mail_app/client/src/App.jsx
+++ Mail_app/client/src/App.jsx
@@ -14,36 +14,41 @@ import EmailTemplateCreator from './pages/campaign/EmailTemplateCreator';
 import SelectTemplate from './pages/campaign/SelectTemplate';
 import Analytics from './pages/Analytics';
 import Admin from './pages/Admin';
+import Developer from './pages/Developer';
 import ProtectedRoute from './components/ProtectedRoute';
-import { Roles } from './roles';
+import { SignedIn } from '@clerk/clerk-react';
+import Feedback from './pages/Feedback';
+import FeedbackList from './pages/FeedbackList';
 
 const App = () => {
+  
   return (
     <Router>
       <div>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/campaign" element={<Campaign />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/create" element={<CampaignForm />} />
-          <Route path="/all-campaigns" element={<CampaignsList />} />
-          <Route path="/create-template" element={<EmailTemplateCreator />} />
-          <Route path="/select-template" element={<SelectTemplate />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route 
-            path="/admin" 
-            element={
-              <ProtectedRoute roles={[Roles.ADMIN]}>
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
         </Routes>
-        <Footer />
+        <SignedIn>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/campaign" element={<Campaign />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/campaign-form" element={<CampaignForm />} />
+            <Route path="/campaign-form/:id" element={<CampaignForm />} />
+            <Route path="/all-campaigns" element={<CampaignsList />} />
+            <Route path="/create-template" element={<EmailTemplateCreator />} />
+            <Route path="/select-template" element={<SelectTemplate />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/admin" element={<ProtectedRoute roles={['admin']}><Admin /></ProtectedRoute>} />
+            <Route path="/developer" element={<ProtectedRoute roles={['developer']}><Developer /></ProtectedRoute>} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/feedbacklist" element={<FeedbackList />} />
+          </Routes>
+        </SignedIn>
+     <Footer />
       </div>
     </Router>
   );
