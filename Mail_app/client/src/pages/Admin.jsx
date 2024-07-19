416ae86f6db29c583d8b0f093509b678868129fe
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-19 07:36:17 +0530

    changes

--- Mail_app/client/src/pages/Admin.jsx
+++ Mail_app/client/src/pages/Admin.jsx
@@ -1,21 +1,77 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Box, Toolbar, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Home, People, Feedback } from '@mui/icons-material';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useAuth } from '@clerk/clerk-react';
 import { checkRole } from '../utils/roles';
+import FeedbackList from './FeedbackList'; 
+
+
+const drawerWidth = 240;
 
 const Admin = () => {
+  const { isLoaded, isSignedIn } = useAuth();
   const navigate = useNavigate();
+  const [currentComponent, setCurrentComponent] = useState(null);
 
-  useEffect(() => {
-    if (!checkRole('admin')) {
-      navigate('/');
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isSignedIn || !checkRole('admin')) {
+    return <Navigate to="/" />;
+  }
+
+  const handleItemClick = (item) => {
+    switch (item) {
+      case 'feedback':
+        setCurrentComponent(<FeedbackList />);
+        break;
+      default:
+        setCurrentComponent(null);
+        break;
     }
-  }, [navigate]);
+  };
 
   return (
-    <>
-      <h1>This is the admin dashboard</h1>
-      <p>This page is restricted to users with the 'admin' role.</p>
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <List>
+          <ListItem component='button' onClick={() => navigate('/')} >
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem component='button' >
+            <ListItemIcon>
+              <People />
+            </ListItemIcon>
+            <ListItemText primary="User Management" />
+          </ListItem>
+          <ListItem component='button' onClick={() => handleItemClick('feedback')}>
+            <ListItemIcon>
+              <Feedback />
+            </ListItemIcon>
+            <ListItemText primary="Feedback" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        {currentComponent}
+      </Box>
+    </Box>
   );
 };
 
