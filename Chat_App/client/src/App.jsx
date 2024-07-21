f30380af15c176f09b6c80a326412a2eca7dd83a
PANKAJ SINGH BISHT <pankajsbisht88@gmail.com>
2024-07-21 23:42:14 +0530

    changes

--- Chat_App/client/src/App.jsx
+++ Chat_App/client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import ChatPage from './Pages/ChatPage';
+import './index.css'
+import SignupPage from './Pages/SignupPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </Router>
